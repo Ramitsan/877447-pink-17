bc0d8b2dd8721294069602a8ed04522a02d8b2e1
Ramitsan <ramitsan@gmail.com>
2019-12-09 18:44:11 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
   var navMain = document.querySelector('.main-nav');
   var navToggle = document.querySelector('.main-nav__toggle');
+  var pageHeaderLogo = document.querySelector('.page-header__logo');
 
   navMain.classList.remove('main-nav--nojs');
 
@@ -7,8 +8,10 @@
     if (navMain.classList.contains('main-nav--closed')) {
       navMain.classList.remove('main-nav--closed');
       navMain.classList.add('main-nav--opened');
+      pageHeaderLogo.classList.add('page-header__logo--open');
     } else {
       navMain.classList.add('main-nav--closed');
       navMain.classList.remove('main-nav--opened');
+      pageHeaderLogo.classList.remove('page-header__logo--open');
     }
   });
