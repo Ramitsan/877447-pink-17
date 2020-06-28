5492578922dd1cdaf4c0994413d2316280956137
Ramitsan <ramitsan@gmail.com>
2020-06-28 19:40:12 +0300

    Добавляет оверлей

--- source/js/script.js
+++ source/js/script.js
@@ -23,6 +23,7 @@
 var inputPhoneElement = document.querySelector('.form-contacts__input--phone');
 var inputEmailElement = document.querySelector('.form-contacts__input--email');
 // var buttonSubmitForm = document.querySelector('.form__btn-submit');
+var modalOverlay = document.querySelector('.modal-overlay');
 var formContest = document.querySelector('.contest__form');
 var modalError = document.querySelector('.modal-bad');
 var modalRequest = document.querySelector('.modal-request');
@@ -44,6 +45,14 @@ var modalErrorClose = function() {
   modalError.classList.remove('modal--show');
 }
 
+var overlayShow = function() {
+  modalOverlay.classList.add('modal--show');
+}
+
+var overlayRemove = function() {
+  modalOverlay.classList.remove('modal--show');
+}
+
 
 var validateForm = function(elem1, elem2) {
   if (elem1.value === '') {
@@ -62,14 +71,12 @@ var validateForm = function(elem1, elem2) {
 
  if (elem1.value !== '' && elem2.value !== '') {
     modalRequestShow();
-    // overlayShowHandler();
-    // disableScrollHandler();
-    // modalRequestCallClose();
   }
 }
 
 formContest.addEventListener('submit', function(evt) {
   evt.preventDefault();
+  overlayShow();
   validateForm(inputPhoneElement, inputEmailElement);
   console.log('111');
 })
